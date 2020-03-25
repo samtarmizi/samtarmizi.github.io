b16d625f4b3c395f8981c6cd92b12d609461222a
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-03-25 11:57:56 +0800

    add UI profile

---
+++ data.js
@@ -0,0 +1,45 @@
+const main = {
+  name: 'Tarmizi Sanusi',
+  mail: 'tarmizi@mizi.my',
+  img: 'https://pbs.twimg.com/profile_images/930725240765759489/wX0dmZw4.jpg',
+  role: ['Software Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'paper-plane-outline',
+      link: 'https://t.me/tarmizisanusi'
+    },
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://samtarmizi.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/samtarmizi'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/tarmizisanusi/'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/sxmtarmizi'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'realtarmizisanusi@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: '(LEMP stack)', link: '#LEMP' },
+    { name: 'Laravel', link: '#Laravel' },
+    { name: 'Flutter', link: '#Flutter' },
+    { name: 'Ionic', link: '#Ionic' }
+  ]
+};
